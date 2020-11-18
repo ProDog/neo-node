46d4d4d3ec94ea41cf78f5d1360bebdee134fd83
Erik Zhang <erik@neo.org>
2020-11-18 23:17:19 +0800

    Add services to plugin system (#679)

--- neo-cli/CLI/MainService.cs
+++ neo-cli/CLI/MainService.cs
@@ -341,6 +341,8 @@ namespace Neo.CLI
                         break;
                 }
 
+            Plugin.AddService(this);
+
             _ = new Logger();
 
             NeoSystem = new NeoSystem(Settings.Default.Storage.Engine);
